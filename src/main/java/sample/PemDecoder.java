374a59da6cca3b7296a559f5cda01ebe82022ff4
Michael <mike.segergmail.com>
2020-11-18 05:27:21 +0100

    added validation

--- src/main/java/sample/PemDecoder.java
+++ src/main/java/sample/PemDecoder.java
@@ -1,14 +1,21 @@
 package sample;
 
 import lombok.extern.slf4j.Slf4j;
+import no.difi.certvalidator.Validator;
+import no.difi.certvalidator.ValidatorBuilder;
+import no.difi.certvalidator.api.CertificateValidationException;
+import no.difi.certvalidator.rule.CRLRule;
+import no.difi.certvalidator.rule.ExpirationRule;
+import no.difi.certvalidator.rule.OCSPRule;
+import no.difi.certvalidator.rule.SigningRule;
 import org.cryptacular.util.CertUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.security.cert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class PemDecoder {
@@ -28,11 +35,27 @@ public class PemDecoder {
         }
     }
 
-    public void decode(File pemFile) throws CertificateException, IOException {
+    private void decode(File pemFile) throws CertificateException, IOException {
         FileInputStream inputStream  =  new FileInputStream (pemFile);
         CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
         X509Certificate cert = (X509Certificate)certFactory.generateCertificate(inputStream);
+        validate(cert);
         String cn = CertUtil.subjectCN(cert);
         log.info("Common Name: {}", cn);
+    }
+
+    private void validate(X509Certificate cert) {
+        Validator validator = ValidatorBuilder.newInstance()
+            .addRule(new ExpirationRule())
+            .addRule(new SigningRule())
+            .addRule(new CRLRule())
+            // .addRule(new OCSPRule())
+            .build();
+        try {
+            validator.validate(cert);
+            log.info("Certificate is valid.");
+        } catch (CertificateValidationException e) {
+            log.error("Certificate validation failed: {}", e.getMessage());
+        }
     }
 }
